b0e589edaa6e61ac3c24bcd64c1e7f1e50e32242
August <august.eymann@gmail.com>
2019-11-04 21:30:16 -0800

    Added Public Folder

---
+++ public/index.js
@@ -0,0 +1,43 @@
+let url_string = window.location;
+let url = new URL(url_string);
+let token = url.searchParams.get("token")
+
+if (token && !window.localStorage.getItem('token')) {
+	window.localStorage.setItem('token', token)
+}
+
+if (window.localStorage.getItem('token')) {
+	console.log('Fetching User Data...')
+	fetch(`https://discordapp.com/api/users/@me`,
+	  {
+		method: 'GET',
+		headers: {
+		  Authorization: `Bearer ${window.localStorage.getItem('token')}`,
+		},
+	  }) .then(response => response.json())
+	  .then(json => {
+		let user = json
+		console.log(user.id)
+
+		document.getElementById('tagline').innerText = `Welcome to my Website, ${user.username}`
+		fetch(`http://localhost:3000/api/roblox/${user.id}`,
+		{
+		  method: 'GET',
+		}) .then(response => response.json())
+		.then(json => {
+		  let user = json
+		  let robloxUsername = json.robloxUsername
+
+		  document.getElementById('roblox').innerText = `ROBLOX USERNAME: ${robloxUsername}`;
+  
+		});
+	  });
+
+} else {
+	console.log('Not Logged In')
+}
+
+function logout() {
+	window.localStorage.removeItem('token');
+	location.reload();
+}
